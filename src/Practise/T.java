5bb852d8031c907da63be30d2fd5d6707dbfed19
oguzky7 <oguzky7@gmail.com>
2022-10-23 15:01:42 -0400

    intellij code push testt

--- src/Practise/T.java
+++ src/Practise/T.java
@@ -1,11 +1,16 @@
 package Practise;
 
+/*
+intellij java push code test
+
+ */
+
 class T {
 
         int t = 20;
     }
     class Main {
-        public static void main(String args[]) {
+        public static void main(String[] args) {
             T t1 = new T();
             System.out.println(t1.t);
         }
